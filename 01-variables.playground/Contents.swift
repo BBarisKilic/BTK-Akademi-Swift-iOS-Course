599eb0c3ada67c833b982118b4d19eb1a766972e
bulentbariskilic@gmail.com <bulentbariskilic@gmail.com>
2021-06-29 23:36:37 +0300

    variables added.

---
+++ 01-variables.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var myName = "Barış"
+var myLastName = "Kılıç"
+
+print(myName + " " + myLastName)
+print("\(myName) \(myLastName)")
+
+myName = "barış"
+
+print(myName)
+
+// comment
+
+var whatIsYourName = "Ali" // camelCase
+var what_is_your_name = "Mehmet" // snake_case
